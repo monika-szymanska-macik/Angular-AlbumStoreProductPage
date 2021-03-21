923568ead738cae23d0b35f35f00666568747e15
Monika <m.szymanska.macik@gmail.com>
2021-03-21 10:53:47 +0100

    Adding a getAlbum Method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,15 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class ProductService {
 
+  private _albumUrl: string = '../assets/album.json';
+
   constructor(private _http: Http) { }
 
+  getAlbum(id: number) {
+
+    return this._http.get(this._albumUrl);
+  }
+
 }
+
+
