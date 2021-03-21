fa69ed30879bc5a39c351c585e1b9f5a09c28481
Monika <m.szymanska.macik@gmail.com>
2021-03-21 13:01:32 +0100

    product list contains dynamic album name

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,8 +21,8 @@ export class ProductService {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
-  getProducts() {
-    return this._http.get(this._productsUrl).map(response => <Product>response.json());
+  getProducts():Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
   }
 
